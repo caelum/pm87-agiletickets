063909a01c78853987573edcdb1137f2b18a5111
7r00 <sheepwintermute@gmail.com>
2014-11-05 10:03:13 -0200

    corrigindo bug de numero de ingressos igual ao numero de vagas

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -38,4 +38,12 @@ public class SessaoTest {
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
 	
+
+	@Test
+	public void deveVender2IngressosSeHa2Vagas() throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(2);
+
+		Assert.assertTrue(sessao.podeReservar(2));
+	}
 }
