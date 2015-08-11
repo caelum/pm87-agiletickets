625f5bb31ccd489b246ea936fa7b4c8397e6a1ea
Andre <ikeziri@gmail.com>
2015-08-11 12:39:02 -0300

    correcao bug de reservas em podeReservar

--- src/main/java/br/com/caelum/agiletickets/models/Sessao.java
+++ src/main/java/br/com/caelum/agiletickets/models/Sessao.java
@@ -108,7 +108,7 @@ public class Sessao {
 
 	public boolean podeReservar(Integer numeroDeIngressos) {
 		int sobraram = getIngressosDisponiveis() - numeroDeIngressos;
-        boolean naoTemEspaco = sobraram <= 0;
+        boolean naoTemEspaco = sobraram < 0;
 
         return !naoTemEspaco;
 	}
