dc14aa2f5fae16853fd0bca88ebc7d133a277279
POcca <paulo.occaso@gmail.com>
2015-08-11 11:19:02 -0300

    inclui Relogio.java

--- src/main/java/br/com/caelum/agiletickets/domain/Relogio.java
+++ src/main/java/br/com/caelum/agiletickets/domain/Relogio.java
@@ -2,9 +2,8 @@ package br.com.caelum.agiletickets.domain;
 
 import org.joda.time.DateTime;
 
-public class RelogioDoSistema implements Relogio {
+public class Relogio {
 
-	@Override
 	public DateTime agora() {
 		return new DateTime();
 	}
