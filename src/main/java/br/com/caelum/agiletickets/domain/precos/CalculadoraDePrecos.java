40f7ad77d457f6c398a6e7e5e7ea9a36727bca99
Andre <ikeziri@gmail.com>
2015-08-12 12:14:29 -0300

    teste ok

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -10,22 +10,30 @@ public class CalculadoraDePrecos {
 	private static final int UMA_HORA = 60;
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
-		BigDecimal preco;
-		preco = sessao.getPreco();
+		BigDecimal preco = sessao.getPreco();
+		
 		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
 			if(calculaFatorDisponivelSobreTotal(sessao) <= 0.05) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+				double fatorReajuste = 0.10;
+				preco = reajustarSessao(sessao, fatorReajuste);
 			} 
 		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
 			if(calculaFatorDisponivelSobreTotal(sessao) <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
+				double fatorReajuste = 0.20;
+				preco = reajustarSessao(sessao, fatorReajuste);
 			} 
 			
 			if(sessao.getDuracaoEmMinutos() > UMA_HORA){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+				double fatorReajuste = 0.10;
+				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(fatorReajuste)));
 			}
 		}  
 		return preco.multiply(BigDecimal.valueOf(quantidade));
+	}
+
+	private static BigDecimal reajustarSessao(Sessao sessao,
+			double fatorReajuste) {
+		return sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(fatorReajuste)));
 	}
 
 	private static double calculaFatorDisponivelSobreTotal(Sessao sessao) {
