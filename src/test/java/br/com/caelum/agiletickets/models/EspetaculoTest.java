ea1c5014787795b60dced8854f89929d2dd0dfab
testee <teste@gmail.com>
2015-08-13 11:25:14 -0300

    Inclusao de testes de espetaculo

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -3,6 +3,19 @@ package br.com.caelum.agiletickets.models;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+
+
+
+
+
+
+
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 import org.junit.Test;
 
 public class EspetaculoTest {
@@ -80,5 +93,36 @@ public class EspetaculoTest {
 
 		return sessao;
 	}
+	
+	@Test
+	public void umaSesao(){
+		LocalDate dataInicio = new LocalDate(2015,8,13);
+		LocalDate dataFim = new LocalDate(2015,8,13);
+		Espetaculo espetaculo = new Espetaculo();
+		LocalTime horario = new LocalTime(20,0,0);
+		List<Sessao> sessoes = espetaculo.criaSessoes(dataInicio, dataFim, horario, Periodicidade.DIARIA);
+		
+		Assert.assertEquals(1, sessoes.size());
+		Assert.assertEquals(dataInicio, sessoes.get(0).getInicio().toLocalDate());
+		Assert.assertEquals("20:00", sessoes.get(0).getHora());
+		Assert.assertEquals(espetaculo, sessoes.get(0).getEspetaculo());	
+	}
+	@Test
+	public void duasSessoes(){
+		LocalDate dataInicio = new LocalDate(2015,8,13);
+		LocalDate dataFim = new LocalDate(2015,8,14);
+		Espetaculo espetaculo = new Espetaculo();
+		LocalTime horario = new LocalTime(20,0,0);
+		List<Sessao> sessoes = espetaculo.criaSessoes(dataInicio, dataFim, horario, Periodicidade.DIARIA);
+		
+		Assert.assertEquals(2, sessoes.size());
+		Assert.assertEquals(dataInicio, sessoes.get(0).getInicio().toLocalDate());
+		Assert.assertEquals(dataInicio, sessoes.get(1).getInicio().toLocalDate());
+		Assert.assertEquals("20:00", sessoes.get(0).getHora());
+		Assert.assertEquals("20:00", sessoes.get(1).getHora());
+		Assert.assertEquals(espetaculo, sessoes.get(0).getEspetaculo());
+		Assert.assertEquals(espetaculo, sessoes.get(1).getEspetaculo());
+	}
+
 	
 }
